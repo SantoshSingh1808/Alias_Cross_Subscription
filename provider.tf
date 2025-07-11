52107fb8d1606af6126134357c653ce71acdf918
Santosh Singh <santosh1808.singh@gmail.com>
2025-07-11 21:00:43 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -27,22 +27,3 @@ provider "azurerm" {
   alias = "Rahu"
 }
 
-provider "azurerm" {
-  # Configuration options
-  features {}
-  subscription_id = var.subscription_id
-  tenant_id = var.tenant_id
-  client_id = var.client_id
-  client_secret = var.client_secret
-  alias = "Sani"
-}
-
-provider "azurerm" {
-  # Configuration options
-  features {}
-  subscription_id = var.subscription_id
-  tenant_id = var.tenant_id
-  client_id = var.client_id
-  client_secret = var.client_secret
-  alias = "Budh"
-}
